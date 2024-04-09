47cdcdc56c9d7b2c7422504561a6a98d71e46a64
LifeSteal <166237187+LlfeSteal@users.noreply.github.com>
2024-04-09 16:01:15 +0200

    Remplacement de la gestion de la config et du listener par PluginFramework

--- src/main/java/fr/lifesteal/respectguard/RespectGuard.java
+++ src/main/java/fr/lifesteal/respectguard/RespectGuard.java
@@ -1,37 +1,34 @@
 package fr.lifesteal.respectguard;
 
-import fr.lifesteal.respectguard.business.*;
+import fr.lifesteal.pluginframework.api.config.ConfigService;
+import fr.lifesteal.pluginframework.core.command.PluginCommand;
+import fr.lifesteal.pluginframework.core.plugin.PluginBase;
+import fr.lifesteal.respectguard.business.ChatGptService;
+import fr.lifesteal.respectguard.business.ChatGuardService;
+import fr.lifesteal.respectguard.business.HttpRequestService;
 import fr.lifesteal.respectguard.business.Interface.IChatGuardService;
-import fr.lifesteal.respectguard.business.config.Interface.IConfigurationService;
-import fr.lifesteal.respectguard.business.config.CacheService;
+import fr.lifesteal.respectguard.business.LoggerService;
 import fr.lifesteal.respectguard.business.config.ConfigurationService;
 import fr.lifesteal.respectguard.business.wrapper.CommandDispatcherWrapper;
 import fr.lifesteal.respectguard.business.wrapper.EventCallerWrapper;
 import fr.lifesteal.respectguard.listener.ChatListener;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginLogger;
-import org.bukkit.plugin.java.JavaPlugin;
 
-public class RespectGuard extends JavaPlugin  {
-    private IConfigurationService configurationService;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RespectGuard extends PluginBase {
+    private ConfigurationService configurationService;
     private IChatGuardService chatGuardService;
 
     @Override
-    public void onEnable() {
-        this.initServices();
-        this.initConfiguration();
-        this.initListeners();
-    }
-
-    private void initListeners() {
-        this.registerListener(new ChatListener(this.chatGuardService));
-    }
-
-    private void initServices() {
+    public void init() {
         var pluginLogger = PluginLogger.getLogger(RespectGuard.class.toString());
         var loggerService = new LoggerService(pluginLogger);
-        var configurationCacheService = new CacheService();
-        this.configurationService = new ConfigurationService(this, this.getConfig(), configurationCacheService);
+
+        var configurationRepository = getConfigRepositoryFactory().getNewYamlRepository("", "config.yml");
+        this.configurationService = new ConfigurationService(getLogger(), configurationRepository);
 
         var httpRequestService = new HttpRequestService(loggerService);
         var chatGptService = new ChatGptService(loggerService, this.configurationService, httpRequestService);
@@ -41,17 +38,22 @@ public class RespectGuard extends JavaPlugin  {
         this.chatGuardService = new ChatGuardService(chatGptService, this.configurationService, eventCaller, commandDispatcher);
     }
 
-    private void initConfiguration() {
-        this.configurationService.initDefaultConfiguration();
-        this.configurationService.loadConfiguration();
-    }
-
-    private void registerListener(Listener listener) {
-        this.getServer().getPluginManager().registerEvents(listener, this);
+    @Override
+    protected List<ConfigService> registerConfigurationsServices() {
+        return new ArrayList<>() {{
+            add(configurationService);
+        }};
     }
 
     @Override
-    public void onDisable() {
+    protected List<Listener> registerListeners() {
+        return new ArrayList<>(){{
+            add(new ChatListener(chatGuardService));
+        }};
+    }
 
+    @Override
+    protected List<PluginCommand> registerCommands() {
+        return new ArrayList<>();
     }
 }
